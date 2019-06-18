c94b33c6bcccfdd9b25615f19ab529901bacaeb3
sherin-shifana <sherinshifana1997@gmail.com>
2019-06-18 18:00:00 +0530

    second

--- main.js
+++ main.js
@@ -22,48 +22,98 @@ H5P.NewContent = (function ($, UI) {
     return this.$card;
   }
 
-  NewContent.prototype.createAnswerCard = function (index) {
-    const that = this;
-    for (let i=0;i<this.options.questions[index].answers.length;i++) {
-
+  NewContent.prototype.createAnswerCard = function (index, answerIndex) {
+      const that = this;
       this.$answercard = $('<div class="answer-card"></div>');
-      this.$answerText = $('<div class="answer-text">'+this.options.questions[index].answers[i].text+'</div>');
-      const hasImage = this.options.questions[index].answers[i].image.file ? true : false;
+      this.$answerText = $('<div class="answer-text">'+this.options.questions[index].answers[answerIndex].text+'</div>');
+      const hasImage = this.options.questions[index].answers[answerIndex].image.file ? true : false;
       console.log(hasImage);
       if (hasImage) {
-        const path = H5P.getPath(this.options.questions[index].answers[i].image.file.path,this.id);
+        const path = H5P.getPath(this.options.questions[index].answers[answerIndex].image.file.path,this.id);
         this.$answerImage = $('<img>', {
               'class': 'answer-image',
               'src': path
-        }).appendTo(this.$answerCard);
+        });
       }
+      that.$answerImage.appendTo(this.$answercard);
+      that.$answerText.appendTo(this.$answercard);
 
-    }
-
-    that.$answerText.appendTo(this.$answerCard);
-
-    return this.$answerCard;
+      return this.$answercard;
   };
 
-  NewContent.prototype.createQuestionCard = function (index) {
+  NewContent.prototype.createQuestionCard = function () {
     const that = this;
-    that.createAnswerCard(index);
-    this.$questionCard = $('<div class="question-card"></div>');
-    this.$question = $('<div class="question">'+this.options.questions[index].text+'</div>');
-    this.$answerDiv = $('<div class="answer-div"></div>');
-    that.$answerCard.appendTo(this.$answerDiv);
-    this.$question.appendTo(this.$questionCard);
-    this.$answerDiv.appendTo(this.$questionCard);
+    this.$questionCards = [];
+    for (let j=0; j<this.options.questions.length; j++){
+      this.$questionCard = $('<div class="question-card"></div>');
+      this.$question = $('<div class="question">'+this.options.questions[j].text+'</div>');
+      this.$question.appendTo(this.$questionCard);
+      for (let i=0;i<this.options.questions[j].answers.length;i++) {
+        this.$answerDiv = $('<div class="answer-div"></div>');
+        that.createAnswerCard(j, i);
+        that.$answercard.appendTo(this.$answerDiv);
+        this.$answerDiv.appendTo(this.$questionCard);
+        this.$answerDiv.click(function() {
+          if (that.currentIndex < that.$questionCards.length-1) {
+            that.currentIndex = that.currentIndex + 1;
+            that.showQuestion();
+          }
+          else if (that.currentIndex === that.$questionCards.length-1) {
+            that.showFinalScreen();
+          }
+        });
+      }
 
-    return this.$questionCard;
+      this.$questionCards.push(this.$questionCard);
+    }
+
+    return this.$questionCards;
+  };
+
+  NewContent.prototype.showQuestion = function () {
+    const that = this;
+    if (that.currentIndex > 0) {
+          this.prevIndex = that.currentIndex - 1;
+          that.$questionCards[this.prevIndex].remove();
+    }
+    if (that.currentIndex !== that.$questionCards.length) {
+          this.nextIndex = that.currentIndex + 1;
+    }
+    that.$questionCards[that.currentIndex].appendTo(that.$wrapper);
   };
 
   NewContent.prototype.startGame = function () {
     const that = this;
-    for(let i=0;i<this.options.questions.length;i++){
-        that.createQuestionCard(i);
-    }
-    that.$questionCard.appendTo(that.$wrapper);
+    this.currentIndex = 0;
+    that.createQuestionCard();
+    that.showQuestion();
+    this.$prevButton = UI.createButton({
+      title: 'previous',
+      html:'<i class="fa fa-angle-double-left" aria-hidden="true"></i>',
+      'class': 'prev-button',
+      click: function(){
+        if (that.currentIndex > 0) {
+          that.$questionCards[that.currentIndex].remove();
+          that.currentIndex = that.currentIndex - 1;
+          that.showQuestion();
+        }
+
+      }
+    }).appendTo(that.$wrapper);
+
+    this.$nextButton = UI.createButton({
+      title: 'start',
+      html:'<i class="fa fa-angle-double-right" aria-hidden="true"></i>',
+      'class': 'next-button',
+      click: function(){
+        console.log(that.$questionCards.length);
+        if (that.currentIndex < that.$questionCards.length-1) {
+          that.currentIndex+=1;
+          that.showQuestion();
+        }
+      }
+    }).appendTo(that.$wrapper);
+    that.createAnswerCard(that.$currentIndex, 0);
   };
 
   NewContent.prototype.attach = function ($container) {
